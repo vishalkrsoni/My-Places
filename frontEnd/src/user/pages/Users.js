414606e6f58b06b06e2efdf56f84c101b57e54b7
vishalkrsoni <vishal.sony1@gmail.com>
2022-12-26 02:16:54 +0530

    new

--- frontEnd/src/user/pages/Users.js
+++ frontEnd/src/user/pages/Users.js
@@ -6,7 +6,18 @@ const Users = () => {
     {
     id: 'u1',
     name: 'vish',
-    image: "v",
+    image: "https://cdn.pixabay.com/photo/2016/03/21/23/25/link-1271843_1280.png",
+    places: 3
+  },
+  {
+    id: 'u1',
+    name: 'vish',
+    image: "https://cdn.pixabay.com/photo/2016/03/21/23/25/link-1271843_1280.png",
+    places: 3
+  }, {
+    id: 'u1',
+    name: 'vish',
+    image: "https://cdn.pixabay.com/photo/2016/03/21/23/25/link-1271843_1280.png",
     places: 3
   }
 ]
